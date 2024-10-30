3089f9cd9766b2004927975fcb0d69c373ba93ed
Liza <lizaapostol378@gmail.com>
2024-10-30 13:30:55 +0200

    update

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export function fetchImages(searchValue) {
+  const API_KEY = '46805132-7b6e04e5dd5e788bcfb9e4740';
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: searchValue,
+  });
+
+  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+import { fetchImages } from './js/pixabay-api';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let searchValue = event.currentTarget.elements.search.value.trim();
+
+  if (!searchValue) {
+    return;
+  }
+  fetchImages(searchValue)
+    .then(data => {
+      if (data.total === 0) {
+        iziToast.error({
+          title: 'Error',
+          message: 'Illegal operation',
+        });
+      } else {
+        console.log(data);
+      }
+    })
+    .catch(data => {
+      console.log(error);
+    });
+}
