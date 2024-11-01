0b0eb09be104431131712c01ea00dfe5396bbcea
Liza <lizaapostol378@gmail.com>
2024-11-01 17:47:08 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 'use strict';
 
 import { fetchImages } from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import icon from './img/error.svg';
+
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
+const loader = document.getElementById('loader');
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 form.addEventListener('submit', handleSubmit);
 
@@ -18,18 +31,37 @@ function handleSubmit(event) {
   if (!searchValue) {
     return;
   }
+  gallery.innerHTML = '';
+
+  loader.classList.remove('hidden');
+
   fetchImages(searchValue)
     .then(data => {
       if (data.total === 0) {
         iziToast.error({
-          title: 'Error',
-          message: 'Illegal operation',
+          backgroundColor: '#ef4040',
+          position: 'topRight',
+          titleColor: '#fff',
+          message:
+            'Sorry, there are no images matching your search query. Please, try again!',
+          messageColor: '#fff',
+          messageSize: '16',
+          iconColor: '#fff',
+          iconUrl: icon,
+          timeout: 3000,
+          maxWidth: 432,
         });
       } else {
-        console.log(data);
+        createMarkup(data);
+        lightbox.refresh();
       }
     })
-    .catch(data => {
+    .catch(error => {
       console.log(error);
+    })
+    .finally(() => {
+      loader.classList.add('hidden');
     });
+
+  event.currentTarget.elements.search.value = '';
 }
